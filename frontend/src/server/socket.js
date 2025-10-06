a543cbcb26f19eef69f620b322bbdd9b3891ca78
Amiya Maity <86618855+Amiya-Maity@users.noreply.github.com>
2025-10-06 15:16:23 +0530

    Update socket.js

--- frontend/src/server/socket.js
+++ frontend/src/server/socket.js
@@ -4,8 +4,6 @@ import { io, Socket } from "socket.io-client";
 const SOCKET_URL = "https://livepoll-nine.vercel.app/"; // your backend server
 
 // Create a singleton socket instance
-const socket = io(SOCKET_URL, {
-  autoConnect: false, // manually control when to connect
-});
+const socket = io(SOCKET_URL);
 
 export default socket;
