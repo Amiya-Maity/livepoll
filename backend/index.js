ad946f7e19a4ca247e81e4fde0c4bc434090cf26
Amiya Maity <86618855+Amiya-Maity@users.noreply.github.com>
2025-10-06 15:18:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const server = app.listen(port, () => {
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: 'https://livepoll-547w.vercel.app/' ,
+        origin: '*' ,
     }
 })
 
