468df036439a2ec8374199880a0f208e33dac08a
Amiya Maity <86618855+Amiya-Maity@users.noreply.github.com>
2025-10-06 15:20:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const { randomUUID } = require('crypto');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
